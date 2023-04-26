0369846ef38e2ca3bfd88ab6217bb70c90d3e9f3
varunvijayan <vknppcd@gmail.com>
2023-04-26 19:02:26 +0530

    add

---
+++ project/portfolio/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from portfolio import views
+
+urlpatterns = [
+    path('',views.home,name='home'),
+    path('about',views.about,name='about'),
+    path('contact',views.contact,name='contact'),
+    path('blog',views.handleblog,name='handleblog'),
+    path('internshipdetails',views.internshipdetails,name='internshipdetails'),
+    path('scertificate',views.scertificate,name='scertificate'),
+    path('projects',views.projects,name='projects'),
+    
+    
+    
+]
